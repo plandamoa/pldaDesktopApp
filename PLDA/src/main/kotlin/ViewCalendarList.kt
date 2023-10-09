7739fad91a4c65ef1a90f9a299ffd5ef49a38238
Jinseo Jeong <js020315@gmail.com>
2023-10-09 16:50:09 +0900

    [FEAT] Select Date Dialog
    날짜 버튼 클릭 시 dialog 표시

---
+++ PLDA/src/main/kotlin/ViewCalendarList.kt
@@ -0,0 +1,25 @@
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun viewCalendarList(onDialogDismiss: () -> Unit) {
+
+    AlertDialog(
+            modifier = Modifier.size(600.dp, 400.dp),
+            onDismissRequest = onDialogDismiss,
+            title = { },
+            text = { },
+            confirmButton = {
+                Button(onClick = onDialogDismiss) {
+                    Text("OK")
+                }
+            }
+    )
+}
