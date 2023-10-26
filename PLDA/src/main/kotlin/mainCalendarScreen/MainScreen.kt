481c721f70be18f33f6931169b8c9b5eee15b3a4
Jinseo Jeong <js020315@gmail.com>
2023-10-26 14:31:22 +0900

    [FEAT] MainCalendarScreen
    설정 버튼 누르면 설정 페이지로 이동
    + 설정 페이지 이미지 아이콘 추가

--- PLDA/src/main/kotlin/mainCalendarScreen/MainScreen.kt
+++ PLDA/src/main/kotlin/mainCalendarScreen/MainScreen.kt
@@ -19,17 +19,18 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import datePickerDialog.DatePickerDialog
+import settingScreen.SettingScreen
 
 @Composable
 fun AppUI() { // 툴바와 달력 레이아웃
     val year = 2023 // 현재 년도
     val month = 12 //현재 월
     Column(
-            modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.White)
-                    .padding(16.dp)
-                    .padding(start = 8.dp, end = 8.dp)
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(16.dp)
+            .padding(start = 8.dp, end = 8.dp)
     ) {
         TopAppBarLayout(year = year, month = month) // 상단 툴바
 
@@ -42,8 +43,8 @@ fun AppUI() { // 툴바와 달력 레이아웃
 @Composable
 fun TopAppBarLayout(year: Int, month: Int) {
     Box(
-            modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp),
-            contentAlignment = Alignment.Center
+        modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp),
+        contentAlignment = Alignment.Center
     ) {
         // 왼쪽: 로고 아이콘과 "PLDA" 텍스트
         TopAppBarLeft()
@@ -60,17 +61,17 @@ fun TopAppBarLayout(year: Int, month: Int) {
 @Composable
 fun TopAppBarLeft() {
     Box(
-            modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp),
+        modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp),
     ) {
         Row(
-                modifier = Modifier.align(Alignment.CenterStart), // 왼쪽 정렬
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                verticalAlignment = Alignment.CenterVertically
+            modifier = Modifier.align(Alignment.CenterStart), // 왼쪽 정렬
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Image(
-                    painter = painterResource("image/plda.png"),
-                    contentDescription = "PLDA Logo",
-                    modifier = Modifier.size(24.dp)
+                painter = painterResource("image/plda.png"),
+                contentDescription = "PLDA Logo",
+                modifier = Modifier.size(24.dp)
             )
             Text(
                 text = "PLDA",
@@ -88,30 +89,30 @@ fun TopAppBarCenter(year: Int, month: Int) {
     var showDatePickerDialog by remember { mutableStateOf(false) }
 
     Box(
-            modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp),
-            contentAlignment = Alignment.Center
+        modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp),
+        contentAlignment = Alignment.Center
     ) {
         Row(
-                modifier = Modifier
-                        .align(Alignment.Center)
-                        .clickable(onClick = { showDatePickerDialog = true }),
-                horizontalArrangement = Arrangement.spacedBy(4.dp),
-                verticalAlignment = Alignment.CenterVertically
+            modifier = Modifier
+                .align(Alignment.Center)
+                .clickable(onClick = { showDatePickerDialog = true }),
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Text(
-                    text = "${year}년 ${month}월",
-                    fontFamily = suitFamily,
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 18.sp,
-                    color = text_primary
+                text = "${year}년 ${month}월",
+                fontFamily = suitFamily,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 18.sp,
+                color = text_primary
             )
             Icon(
-                    painterResource("image/expand_more.svg"),
-                    contentDescription = "Drop Down Arrow",
-                    tint = gray_40,
-                    modifier = Modifier
-                            .size(16.dp)
-                            .padding(start = 4.dp)
+                painterResource("image/expand_more.svg"),
+                contentDescription = "Drop Down Arrow",
+                tint = gray_40,
+                modifier = Modifier
+                    .size(16.dp)
+                    .padding(start = 4.dp)
             )
         }
         if (showDatePickerDialog) {
@@ -123,34 +124,35 @@ fun TopAppBarCenter(year: Int, month: Int) {
 @Composable
 fun TopAppBarRight() {
     val showAddScheduleDialog = remember { mutableStateOf(false) }
+    val showSettingScheduleDialog = remember { mutableStateOf(false) }
 
     Box(
-            modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp),
+        modifier = Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp),
     ) {
         Row(
-                modifier = Modifier.align(Alignment.CenterEnd),
-                horizontalArrangement = Arrangement.spacedBy(20.dp),
-                verticalAlignment = Alignment.CenterVertically
+            modifier = Modifier.align(Alignment.CenterEnd),
+            horizontalArrangement = Arrangement.spacedBy(20.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             SearchBar()
 
             Box(modifier = Modifier.size(24.dp)) {
                 Icon(
-                        Icons.Default.Add,
-                        contentDescription = "Add",
-                        modifier = Modifier
-                                .size(24.dp)
-                                .clickable(onClick = { showAddScheduleDialog.value = true })
+                    Icons.Default.Add,
+                    contentDescription = "Add",
+                    modifier = Modifier
+                        .size(24.dp)
+                        .clickable(onClick = { showAddScheduleDialog.value = true })
                 )
             }
 
             Box(modifier = Modifier.size(24.dp)) {
                 Icon(
-                        painterResource("image/setting.svg"),
-                        contentDescription = "Settings",
-                        modifier = Modifier
-                                .size(24.dp)
-                                .clickable(onClick = {  })
+                    painterResource("image/setting.svg"),
+                    contentDescription = "Settings",
+                    modifier = Modifier
+                        .size(24.dp)
+                        .clickable(onClick = { showSettingScheduleDialog.value = true })
                 )
             }
         }
@@ -159,6 +161,12 @@ fun TopAppBarRight() {
         AddScheduleScreen(
             isAddScreenVisible = showAddScheduleDialog,
             onDismiss = { showAddScheduleDialog.value = false }
+        )
+    }
+    if (showSettingScheduleDialog.value) {
+        SettingScreen(
+            isSettingScreenVisible = showSettingScheduleDialog,
+            onDismiss = { showSettingScheduleDialog.value = false }
         )
     }
 }
