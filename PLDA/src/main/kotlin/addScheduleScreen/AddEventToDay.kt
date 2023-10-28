b9014b5f6495b81a949398d607495bddfb6f0714
Jinseo Jeong <js020315@gmail.com>
2023-10-28 20:02:47 +0900

    하드코딩 일정 추가

--- PLDA/src/main/kotlin/addScheduleScreen/AddEventToDay.kt
+++ PLDA/src/main/kotlin/addScheduleScreen/AddEventToDay.kt
@@ -1,9 +1,14 @@
 package addScheduleScreen
 
 val events = mutableMapOf<String, MutableList<String>>(
-    "2023-10-19" to mutableListOf("직장", "운동하기", "외출"),
-    "2023-10-21" to mutableListOf("공부하기"),
-    "2023-10-22" to mutableListOf("메일을 통해 내일 오후 2시에 진행될 프리코스 오리엔테이션 일정")
+    "2023-10-19" to mutableListOf("알고리즘 공부", "헬스장 PT", "외출"),
+    "2023-10-21" to mutableListOf("프로그래밍 과제", "카공"),
+    "2023-10-22" to mutableListOf("외부 대회 회의", "장 보기"),
+    "2023-10-04" to mutableListOf("대외활동 1차 발표"),
+    "2023-12-02" to mutableListOf("저녁 약속", "운동하기", "외출"),
+    "2023-12-10" to mutableListOf("치과", "친구 만나기"),
+    "2023-12-15" to mutableListOf("자격증 시험 점수 공개"),
+    "2023-12-13" to mutableListOf("종강총회")
 )
 
 fun addEventToDay(date: String, event: String) {
